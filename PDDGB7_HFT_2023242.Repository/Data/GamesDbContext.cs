03e56a4b2255ab07cdc3de0359ddf4bc433b9729
Worzy1 <dbalazs02@gmail.com>
2024-05-02 17:15:36 +0200

    Developer is done

--- PDDGB7_HFT_2023242.Repository/Data/GamesDbContext.cs
+++ PDDGB7_HFT_2023242.Repository/Data/GamesDbContext.cs
@@ -67,24 +67,18 @@ namespace PDDGB7_HFT_2023242.Repository.Data
                 new Game("7#3#Minecraft#2011*11*18"),
                 new Game("8#4#Warframe#2013*03*25"),
                 new Game("9#5#StardewValley#2016*02*26"),
-                new Game("10#1#Borderlands#2009*10*20"),
-                new Game("1#1#Borderlands#2009*10*20"),
-                new Game("12#1#Borderlands#2009*10*20"),
-                new Game("13#1#Borderlands#2009*10*20"),
-                new Game("14#1#Borderlands#2009*10*20"),
-                new Game("15#1#Borderlands#2009*10*20"),
-                new Game("16#1#Borderlands#2009*10*20"),
-                new Game("17#1#Borderlands#2009*10*20"),
-                new Game("18#1#Borderlands#2009*10*20"),
-                new Game("19#1#Borderlands#2009*10*20"),
-                new Game("20#1#Borderlands#2009*10*20"),
-                new Game("21#1#Borderlands#2009*10*20"),
-                new Game("22#1#Borderlands#2009*10*20"),
-                new Game("23#1#Borderlands#2009*10*20"),
-                new Game("24#1#Borderlands#2009*10*20"),
-                new Game("25#1#Borderlands#2009*10*20"),
-                new Game("26#1#Borderlands#2009*10*20"),
-
+                new Game("10#6#CSGO#2012*08*21"),
+                new Game("11#6#HalfLife#1998*11*19"),
+                new Game("12#6#HalfLife2#2004*11*16"),
+                new Game("13#1#FarCry5#2018*03*27"),
+                new Game("14#7#FarCry6#2021*10*07"),
+                new Game("15#7#RainbowSixSiege#2015*12*01"),
+                new Game("16#8#ApexLegends#2019*02*04"),
+                new Game("17#9#LeagueOfLegends#2009*10*27"),
+                new Game("18#2#Hearthstone#2014*03*11"),
+                new Game("19#10#Skyrim#2011*11*11"),
+                new Game("20#10#Fallout4#2015*11*10"),
+               
             });;
             modelBuilder.Entity<Developer>().HasData(new Developer[]
             {
@@ -93,11 +87,16 @@ namespace PDDGB7_HFT_2023242.Repository.Data
                 new Developer("3#Mojang"),
                 new Developer("4#DigitalExtremes"),
                 new Developer("5#ConcernedApe"),
-                
-                
-
+                new Developer("6#Valve"),
+                new Developer("7#Ubisoft"),
+                new Developer("8#RespawnEntertainment"),
+                new Developer("9#RiotGames"),
+                new Developer("10#Bethesda"),   
             }); ;
-
+            modelBuilder.Entity<User>().HasData(new User[]
+            {
+                new User()
+            }); ;
         }
 
 
