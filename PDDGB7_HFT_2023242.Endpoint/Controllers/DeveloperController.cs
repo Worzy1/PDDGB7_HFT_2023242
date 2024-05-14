56277275a173e146d72c98fd8096fc64f05dd014
Worzy1 <dbalazs02@gmail.com>
2024-05-14 19:29:51 +0200

    Developer Controller is készz

---
+++ PDDGB7_HFT_2023242.Endpoint/Controllers/DeveloperController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDDGB7_HFT_2023242.Logic.Interfaces;
+using PDDGB7_HFT_2023242.Models;
+using System.Collections.Generic;
+
+
+
+namespace PDDGB7_HFT_2023242.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DeveloperController : ControllerBase
+    {
+        IDeveloperLogic logic;
+
+        public DeveloperController(IDeveloperLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        
+        [HttpGet]
+        public IEnumerable<Developer> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+       
+        [HttpGet("{id}")]
+        public Developer Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+       
+        [HttpPost]
+        public void Create([FromBody] Developer item)
+        {
+             this.logic.Create(item);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Developer item)
+        {
+            this.logic.Update(item);
+        }
+
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
