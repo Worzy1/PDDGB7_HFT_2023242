1046277022df3433d75f6b4de5573453026aa62d
Worzy1 <dbalazs02@gmail.com>
2024-05-14 19:45:19 +0200

    Utolsó controller is készen lett

---
+++ PDDGB7_HFT_2023242.Endpoint/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDDGB7_HFT_2023242.Logic.Interfaces;
+using PDDGB7_HFT_2023242.Models;
+using System.Collections.Generic;
+
+
+namespace PDDGB7_HFT_2023242.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        IUserLogic logic;
+
+        public UserController(IUserLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public User Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] User item)
+        {
+            this.logic.Create(item);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] User item)
+        {
+            this.logic.Update(item);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+
+        [HttpGet("mostLikedDeveloperOf/{refcode}")]
+        public string GetMostLikedDeveloperOfUser(string refcode)
+        {
+
+            return this.logic.GetMostLikedDeveloper(refcode);
+
+        }
+
+        [HttpGet("averageNumOfRents/")]
+        public double GetAllUsersAverageNumberOfRents() => this.logic.GetAllUsersAverageNumberOfRents();
+    }
+}
