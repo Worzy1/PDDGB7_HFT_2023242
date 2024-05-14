60cee61413b2a812a84caedcbe6d20c27a3ea40a
Worzy1 <dbalazs02@gmail.com>
2024-05-14 21:15:04 +0200

    pprogram cs create

--- PDDGB7_HFT_2023242/Program.cs
+++ PDDGB7_HFT_2023242/Program.cs
@@ -7,39 +7,54 @@ namespace PDDGB7_HFT_2023242
 {
     internal class Program
     {
-
+        static RestService rest;
         static void Create(string entityType)
         {
             string id;
             string developerId;
-            //switch (entityType)
-            //{
-            //    case "Developer":
-            //        Console.Write("Enter developer's id: ");
-            //        id = Console.ReadLine();
-            //        Console.Write("Enter developer's name: ");
-            //        string developerName = Console.ReadLine();
+            switch (entityType)
+            {
+                case "Developer":
+                    Console.Write("Enter developer's id: ");
+                    id = Console.ReadLine();
+                    Console.Write("Enter developer's name: ");
+                    string developerName = Console.ReadLine();
+                    rest.Post(new Developer($"{id}#{developerName}"), "Developer");
+                    break;
+                case "Game":
+                    Console.Write("Enter game's id: ");
+                    id = Console.ReadLine();
+                    Console.Write("Enter developer's id: ");
+                    developerId = Console.ReadLine();
+                    Console.Write("Enter game's title: ");
+                    string title = Console.ReadLine();
+                    Console.Write("Enter game's date of release: ");
+                    string yearOfRelease = Console.ReadLine();
+                    rest.Post(new Game($"@{id}#{developerId}#{title}#{yearOfRelease}"), "Game");
+                    break;
+                case "User":
+                    Console.Write("Enter user's id: ");
+                    id = Console.ReadLine();
+                    Console.Write("Enter user's refcode: ");
+                    string userRef = Console.ReadLine();
+                    Console.Write("Enter User's name: ");
+                    string userName = Console.ReadLine();
+                    rest.Post(new User($"{id}#{userName}#{userRef}"), "User");
+                    break;
+                default:
+                    Console.Write("Enter rent log's id: ");
+                    id = Console.ReadLine();
+                    Console.Write("Enter user's id: ");
+                    string userId = Console.ReadLine();
+                    Console.Write("Enter game's id: ");
+                    string gameId = Console.ReadLine();
+                    Console.Write("Enter game's date of release: ");
+                    string dateOfRent = Console.ReadLine();
+                    rest.Post(new Game($"{id}#{userId}#{gameId}#{dateOfRent}"), "RentLog");
+                    break;
 
-            //        break;
-            //    case "Game":
-            //        Console.Write("Enter game's id: ");
-            //        id = Console.ReadLine();
-            //        Console.Write("Enter developer's id: ");
-            //        developerId = Console.ReadLine();
-            //        Console.Write("Enter game's title: ");
-            //        string title = Console.ReadLine();
-            //        Console.Write("Enter game's date of release: ");
-            //        string yearOfRelease = Console.ReadLine();
 
-            //        break;
-            //    case "User":
-            //        Console.Write("Enter user's id: ");
-            //        id = Console.ReadLine();
-            //        Console.Write("Enter User's refcode: ");
-            //        string userRef = Console.ReadLine();
-
-
-            //}
+            }
         }
 
 
